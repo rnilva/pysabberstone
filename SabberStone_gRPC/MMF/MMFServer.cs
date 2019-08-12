9f4d80ae4c3c32c9c1a14dfdbeb8748930509c31
Milva <garangggg@gmail.com>
2019-08-13 03:59:13 +0900

    Fix logging

--- SabberStone_gRPC/MMF/MMFServer.cs
+++ SabberStone_gRPC/MMF/MMFServer.cs
@@ -20,121 +20,124 @@ namespace SabberStone_gRPC.MMF
         public static List<Task> RunningThreads = new List<Task>();
         private static CancellationTokenSource CTS = new CancellationTokenSource();
 
-        public static unsafe void Run(string id = "")
+        public static unsafe void Run(string id = "", bool isTask = false)
         {
-            using (var outputFile = File.CreateText(id + OUTPUT_PATH))
+            StreamWriter outputFile;
+            TextWriter stdout = null;
+            if (!isTask)
             {
+                outputFile = File.CreateText(id + OUTPUT_PATH);
                 outputFile.AutoFlush = true;
-                var stdout = Console.Out;
+                stdout = Console.Out;
                 Console.SetOut(outputFile);
-                while (true)
+            }
+            
+            while (true)
+            {
+                using (var pipe = new NamedPipeServerStream(PIPE_NAME_PREFIX + id, PipeDirection.InOut, 99))
+                using (var mmf = MemoryMappedFile.CreateFromFile(
+                    File.Open(Path.Combine("../", id + MMF_NAME_POSTFIX), FileMode.OpenOrCreate),
+                    null, 10000, MemoryMappedFileAccess.ReadWrite, HandleInheritability.None, false))
+                using (MemoryMappedViewAccessor view = mmf.CreateViewAccessor())
                 {
-                    using (var pipe = new NamedPipeServerStream(PIPE_NAME_PREFIX + id, PipeDirection.InOut, 1))
-                    using (var mmf = MemoryMappedFile.CreateFromFile(
-                        File.Open(Path.Combine("../", id + MMF_NAME_POSTFIX), FileMode.OpenOrCreate),
-                        null, 10000, MemoryMappedFileAccess.ReadWrite, HandleInheritability.None, false))
-                    using (MemoryMappedViewAccessor view = mmf.CreateViewAccessor())
+                    byte* mmfPtr = null;
+                    view.SafeMemoryMappedViewHandle.AcquirePointer(ref mmfPtr);
+
+                    Console.WriteLine("Server started. Waiting for the client.....");
+
+                    pipe.WaitForConnection();
+
+                    Console.WriteLine("Python client connected!");
+                    try
                     {
-                        byte* mmfPtr = null;
-                        view.SafeMemoryMappedViewHandle.AcquirePointer(ref mmfPtr);
-
-                        Console.WriteLine("Server started. Waiting for the client.....");
-
-                        pipe.WaitForConnection();
-
-                        Console.WriteLine("Python client connected!");
-                        try
+                        using (BinaryWriter bw = new BinaryWriter(pipe))
+                        using (BinaryReader br = new BinaryReader(pipe))
                         {
-                            using (BinaryWriter bw = new BinaryWriter(pipe))
-                            using (BinaryReader br = new BinaryReader(pipe))
+                            while (true)
                             {
+                                byte function_id = br.ReadByte();
+
+                                if (function_id == (byte)FunctionId.Terminate)
+                                {
+                                    CTS.Cancel();
+                                    if (!isTask) Console.SetOut(stdout);
+                                    return;
+                                }
+                                else if (function_id == (byte) FunctionId.NewThread)
+                                {
+                                    //int len = br.ReadInt32();
+                                    //string threadId = Encoding.Default.GetString(br.ReadBytes(len));
+                                    int threadId = br.ReadInt32();
+                                    Console.WriteLine($"New Thread {threadId} is created.");
+                                    RunningThreads.Add(
+                                        Task.Factory.StartNew(() => 
+                                            Run(id + threadId, true), CTS.Token));
+                                    bw.Write((byte) 4);
+                                    continue;
+                                }
+
+
+                                //Debug.WriteLine($"Function {function_id} is requested");
+
+                                List<dynamic> arguments = new List<dynamic>();
+
                                 while (true)
                                 {
-                                    byte function_id = br.ReadByte();
-
-                                    if (function_id == (byte)FunctionId.Terminate)
+                                    char type = br.ReadChar();
+                                    if (type == 'i')
+                                        arguments.Add(br.ReadInt32());
+                                    else if (type == 'b')
+                                        arguments.Add(br.ReadBoolean());
+                                    else if (type == 's')
                                     {
-                                        CTS.Cancel();
-                                        Console.SetOut(stdout);
-                                        return;
+                                        int len = br.ReadInt32();
+                                        string str = Encoding.Default.GetString(br.ReadBytes(len));
+                                        arguments.Add(str);
                                     }
-                                    else if (function_id == (byte) FunctionId.NewThread)
+                                    else if (type == 'o')
                                     {
-                                        //int len = br.ReadInt32();
-                                        //string threadId = Encoding.Default.GetString(br.ReadBytes(len));
-                                        int threadId = br.ReadInt32();
-                                        Console.WriteLine($"New Thread {threadId} is created.");
-                                        RunningThreads.Add(
-                                            Task.Factory.StartNew(() => 
-                                                Run(id + threadId), CTS.Token));
-                                        bw.Write((byte) 4);
-                                        continue;
-                                    }
-
-
-                                    //Debug.WriteLine($"Function {function_id} is requested");
-
-                                    List<dynamic> arguments = new List<dynamic>();
-
-                                    while (true)
-                                    {
-                                        char type = br.ReadChar();
-                                        if (type == 'i')
-                                            arguments.Add(br.ReadInt32());
-                                        else if (type == 'b')
-                                            arguments.Add(br.ReadBoolean());
-                                        else if (type == 's')
+                                        var bytes = br.ReadBytes(Option.Size);
+                                        unsafe
                                         {
-                                            int len = br.ReadInt32();
-                                            string str = Encoding.Default.GetString(br.ReadBytes(len));
-                                            arguments.Add(str);
-                                        }
-                                        else if (type == 'o')
-                                        {
-                                            var bytes = br.ReadBytes(Option.Size);
-                                            unsafe
+                                            fixed (byte* ptr = bytes)
                                             {
-                                                fixed (byte* ptr = bytes)
-                                                {
-                                                    Option* opPtr = (Option*)ptr;
-                                                    arguments.Add(*opPtr);
-                                                }
+                                                Option* opPtr = (Option*)ptr;
+                                                arguments.Add(*opPtr);
                                             }
                                         }
-                                        else if (type == '4') // End of Transmission
-                                            break;
-                                        else
-                                        {
-                                            Console.WriteLine("Undefined value is received: " + type);
-                                            break;
-                                        }
-
                                     }
-
-                                    try
+                                    else if (type == '4') // End of Transmission
+                                        break;
+                                    else
                                     {
-                                        int size = FunctionTable.CallById((FunctionId) function_id, arguments, in mmfPtr);
-
-                                        //Debug.WriteLine($"Server writes a structure of size {size} to mmf");
-
-                                        bw.Write(size); // Send the size of returned structure.
+                                        Console.WriteLine("Undefined value is received: " + type);
+                                        break;
                                     }
-                                    catch (Exception e)
-                                    {
-                                        Console.WriteLine(e.Message);
-                                        Console.WriteLine(e.StackTrace);
-                                    }
+
+                                }
+
+                                try
+                                {
+                                    int size = FunctionTable.CallById((FunctionId) function_id, arguments, in mmfPtr);
+
+                                    //Debug.WriteLine($"Server writes a structure of size {size} to mmf");
+
+                                    bw.Write(size); // Send the size of returned structure.
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                    Console.WriteLine(e.StackTrace);
                                 }
                             }
                         }
-                        catch (IOException e)
-                        {
-                            Console.WriteLine("ERROR: " + e.Message);
-                            Console.WriteLine("Connection closed.");
-                        }
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("ERROR: " + e.Message);
+                        Console.WriteLine("Connection closed.");
                     }
                 }
-                //Console.SetOut(stdout);
             }
         }
     }
