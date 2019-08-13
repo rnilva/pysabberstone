efdc61016728c187bdd7f4e1c9e0a7f20bc98541
Milva <garangggg@gmail.com>
2019-08-13 19:00:32 +0900

    Tweak functions

--- SabberStone_python_client/sabber_protocol/function.py
+++ SabberStone_python_client/sabber_protocol/function.py
@@ -4,48 +4,38 @@ from struct import *
 
 def call_function_multiargs(socket, mmf, function_id, *args):
     # Send function id as a byte
-    socket.send(bytes([function_id]))
+    byte = bytes([function_id])
     for arg in args:
         if type(arg) is int:
-            socket.send(b'i')
-            socket.send(pack('i', arg))
+            byte += b'i' + pack('i', arg)
         elif type(arg) is str:
-            socket.send(b's')
-            socket.send(pack('i', len(arg)))
-            socket.send(arg.encode())
-    socket.send(b'4')
+            byte += b's' + pack('i', len(arg)) + arg.encode()
+    socket.send(byte + b'4')
     return _retrieve_returned_value(socket, mmf)
 
 
 def call_function(socket, mmf, function_id, int_arg: int):
-    socket.send(bytes([function_id]))
-    # Send 1 int argument.
-    socket.send(b'i')
-    socket.send(pack('i', int_arg))
-    socket.send(b'4')
+    socket.send(bytes([function_id]) + b'i' + pack('i', int_arg) + b'4')
     return _retrieve_returned_value(socket, mmf)
 
 
 def call_function_void_return(socket, function_id):
     socket.send(bytes([function_id]))
 
+
 def call_function_void_return_int_arg(socket, function_id, int_arg: int):
     socket.send(bytes([function_id]) + pack('i', int_arg))
     eot = socket.recv(1)
 
+
 def send_option(socket, mmf, game_id, option):
-    socket.send(bytes([7]))
-    socket.send(b'i')
-    socket.send(pack('i', game_id))
-    socket.send(b'o')
-    socket.send(bytes(option))
-    socket.send(b'4')
+    socket.send(b'\x07i' + pack('i', game_id) + b'o' + bytes(option) + b'4')
     return _retrieve_returned_value(socket, mmf)
 
 
 def _retrieve_returned_value(socket, mmf):
     size = unpack('I', socket.recv(4))[0]
-    return mmf[0:size].tobytes()
+    return mmf[0:size]
 
 
 def _encode_argument(arg):
