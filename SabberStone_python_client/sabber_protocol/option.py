a329c78b87e2784e44f246eb03b2731b6183ae29
rnilva <garangggg@gmail.com>
2019-08-14 05:41:42 +0900

    First attempt on print

--- SabberStone_python_client/sabber_protocol/option.py
+++ SabberStone_python_client/sabber_protocol/option.py
@@ -18,7 +18,7 @@ class Option:
     size = 20
 
     def __init__(self, data_bytes):
-        fields = unpack(Option.fmt, data_bytes)
+        fields = unpack(Option.fmt, data_bytes[0:size])
         self.type = PlayerTaskType(fields[0])
         (
             self.source_position,
@@ -26,6 +26,9 @@ class Option:
             self.sub_option,
             self.choice
         ) = fields[1:5]
+        length = unpack('i', data_bytes[size:size+4])[0]
+        self.print = data_bytes[size+4:size+4+length].decode()
+        self.size = size + length
 
     def __str__(self):
         return "[{0}] {1} => {2}".format(self.type, self.source_position,
@@ -38,6 +41,11 @@ class Option:
 
 def get_options_list(data_bytes):
     options = []
-    for i in range(0, len(data_bytes), Option.size):
-        options.append(Option(data_bytes[i:i+Option.size]))
+    offset = 0
+    # for i in range(0, len(data_bytes), Option.size):
+    #     options.append(Option(data_bytes[i:]))
+    while offset < len(data_bytes):
+        option = Option(data_bytes[offset:])
+        options.append(option)
+        offset += option.size
     return options
