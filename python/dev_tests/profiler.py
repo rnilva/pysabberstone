21d471fb0b58b7ebd00f24e1c1f9c4b88596115b
Milva <garangggg@gmail.com>
2019-08-16 02:46:30 +0900

    performance profiling

--- python/dev_tests/profiler.py
+++ python/dev_tests/profiler.py
@@ -1,5 +1,6 @@
-import python_pb2
+import pysabberstone.python.rpc.python_pb2
 import random
+import time
 
 
 def random_games_rpc(stub, deck, count):
@@ -13,12 +14,17 @@ def random_games_rpc(stub, deck, count):
         game = stub.Reset(game_id)
         print("{0}th game is finished.".format(i + 1))
 
+
 def random_games_mmf(server, deck, count):
+    process_time = 0
     game = server.new_game(deck, deck)
     for i in range(count):
         while game.state != 3:
             options = server.options(game)
             option = options[random.randrange(len(options))]
+            t = time.time()
             game = server.process(game, option)
+            process_time += time.time() - t
         game = server.reset(game)
         print("{0}th game is finished.".format(i + 1))
+    print("Process time: ", process_time)
