62b4f706b97a67b752a9c88f7c9d68824756e2d9
rnilva <garangggg@gmail.com>
2019-08-28 02:01:24 +0900

    Re-added Option.print and Added Option.is_spell in rpc server

--- python/rpc/test.py
+++ python/rpc/test.py
@@ -2,13 +2,11 @@ import grpc
 import python_pb2
 import python_pb2_grpc
 import random
-import profiler
 import cProfile
 import re
-from run_server import run_server as run_server
 # from google.protobuf import empty_pb2
 
-# python -m grpc_tools.protoc -I../SabberStone_gRPC/Protos --python_out=. --grpc_python_out=. ../SabberStone_gRPC/Protos/python.proto
+# python -m grpc_tools.protoc -I../../dotnet_core/Protos --python_out=. --grpc_python_out=. ../../dotnet_core/Protos/python.proto
 
 
 def full_random_game(stub, deck1, deck2):
@@ -17,6 +15,8 @@ def full_random_game(stub, deck1, deck2):
     options_list = []
     while game.state != 3:
         options = stub.GetOptions(id)
+        for opt in options.list:
+            print(opt.print)
         option = options.list[random.randrange(len(options.list))]
         game = stub.Process(option)
         print("Turn: {0}".format(game.turn))
@@ -43,7 +43,7 @@ string1 = r"AAECAa0GBKbwAr3zApeHA+aIAw3lBOEH9geNCKUJ0gryDPsM5fcC0P4C0okD64oDz50D
 # https://www.hearthpwn.com/decks/1286917-starter-pack-mage
 string2 = r"AAECAf0EAA8MuwKVA6sEtATmBJYFhQjC8wK0/ALnlQOmmAOfmwP/nQPinwMA"
 
-# game = stub.NewGame(python_pb2.DeckStrings(deck1=string1, deck2=string2))
+game = stub.NewGame(python_pb2.DeckStrings(deck1=string1, deck2=string2))
 
 # player1 = game.CurrentPlayer
 # player2 = game.CurrentOpponent
@@ -59,17 +59,15 @@ string2 = r"AAECAf0EAA8MuwKVA6sEtATmBJYFhQjC8wK0/ALnlQOmmAOfmwP/nQPinwMA"
 #     print('\t' + cards.cards[card.card_id].name)
 
 
-# options = stub.GetOptions(game.id)
-# option_list = []
+options = stub.GetOptions(game.id)
 
-# for option in options.list:
-#     print(option.print)
-#     option_list.append(option)
+for option in options.list:
+    print(option.print)
 
 
 # game = stub.Process(option_list[0])
 
-# full_random_game(stub, string1, string2)
-stats = cProfile.run('profiler.random_games(stub, r"AAEBAf0EAA8MLU1xwwG7ApUDrgO/A4AEtATmBO0EoAW5BgA=", 100)')
+full_random_game(stub, string1, string2)
+# stats = cProfile.run('profiler.random_games(stub, r"AAEBAf0EAA8MLU1xwwG7ApUDrgO/A4AEtATmBO0EoAW5BgA=", 100)')
 
 # profiler.random_games(stub, r"AAEBAf0EAA8MLU1xwwG7ApUDrgO/A4AEtATmBO0EoAW5BgA=", 100)
