0a0ae2c6499423a304f42a7f2eb0a30da812a987
Milva <garangggg@gmail.com>
2019-11-26 14:56:43 +0900

    - Refactor solution and project name SabberStone_gRPC -> SabberStonePython - Add FindAI - Fix proto

--- dotnet_core/Properties/Resources.Designer.cs
+++ dotnet_core/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SabberStone_gRPC.Properties {
+namespace SabberStonePython.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SabberStone_gRPC.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SabberStone_gRPC.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SabberStonePython.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
